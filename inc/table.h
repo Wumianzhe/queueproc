602c4a11cc30cbab26b2b37f890d8759ace214bb
Kopnov Alexandr <a.kopnov24@gmail.com>
2023-09-07 10:16:39 +0300

    initial table

--- inc/table.h
+++ inc/table.h
@@ -2,13 +2,22 @@
 #define TABLE_H_
 
 #include <chrono>
+#include <string>
 
 class table {
-    bool full;
-    std::chrono::minutes full_since;
+  bool full = false;
+  std::chrono::minutes full_since;
 
-    int bill_hours;
-    std::chrono::minutes utilization;
+  int bill_hours = 0;
+  std::chrono::minutes utilization{0};
+
+public:
+  int getBillableHours();
+  std::chrono::minutes getUtilization();
+  bool isFull();
+  // naming things is hard
+  void startJob(std::chrono::minutes curr_time);
+  void endJob(std::chrono::minutes curr_time);
 };
 
 #endif // TABLE_H_
---
+++ src/table.cpp
@@ -0,0 +1,18 @@
+#include "table.h"
+
+int table::getBillableHours() { return bill_hours; }
+
+void table::endJob(std::chrono::minutes curr_time) {
+  utilization += curr_time - full_since;
+  bill_hours +=
+      std::chrono::ceil<std::chrono::hours>(curr_time - full_since).count();
+}
+
+std::chrono::minutes table::getUtilization() { return utilization; }
+
+void table::startJob(std::chrono::minutes curr_time) {
+  full = true;
+  full_since = curr_time;
+}
+
+bool table::isFull() { return full; }
